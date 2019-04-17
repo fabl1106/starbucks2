c9909486fbe6296505b7b0fa2c2c814a6c13faaf
kimnamhyung <lutorydream@gmail.com>
2019-04-17 12:09:03 +0900

    hehe
    he

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+for x in range(1,101):
+    if x % 15==0:
+        print('15의배수')
+    elif x % 5==0:
+        print('5의배수')
+    elif x % 3==0:
+        print('3의배수')
+    else:
+        print(x)
+
